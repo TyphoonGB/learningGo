adc68fd2b59c1c055c4986501b1821948038fe82
[German Bundur] <gebundur@gmail.com>
2024-11-30 17:32:57 +0100

    arrays and slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,17 +1,45 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 3, 3, 7}
+	t.Run("collections of any size", func(t *testing.T) {
+
+		numbers := []int{1, 2, 3}
 
 		got := Sum(numbers)
-		want := 14
+		want := 6
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("collection of any size", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{1, 3, 3, 7})
+		want := []int{5, 13}
+		checkSums(t, got, want)
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 3, 3, 7})
+		want := []int{0, 13}
+		checkSums(t, got, want)
+	})
+
 }
